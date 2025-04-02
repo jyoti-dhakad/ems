3dee4b2be66691fb6c928eecf8de44227ff8e54f
Jyoti-Dhakad <dhakadjyoti130@gmail.com>
2025-04-02 16:05:52 +0530

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  # Admin Panel ko root route banane ke liye
+  root to: "admin/dashboard#index"
+
   namespace :admin do
     resources :leaves do
       member do
